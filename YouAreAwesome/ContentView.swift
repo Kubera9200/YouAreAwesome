b85eb0d3a148d5819d661ca4f2f1fceeeb3cf0d8
Likhith Areekkal <li.areekkal@gmail.com>
2023-09-20 23:44:12 -0400

    2023-09-20-11:42p

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -12,41 +12,61 @@ struct ContentView: View {
 //    @State private var intCount = 1
     
     var body: some View {
-        VStack (spacing: 4) {
-            Text("You have Skills!")
-                .font(.largeTitle)
-                .fontWeight(.black)
-                .background(.purple)
-                .cornerRadius(10)
-            Spacer()
-            Text(strMessage)
-                .font(.largeTitle)
-                .fontWeight(.heavy)
-                .minimumScaleFactor(0.5)
-                .multilineTextAlignment(.center)
-                .foregroundColor(Color.red)
-                .frame(height: 150)
-                .frame(maxWidth: .infinity)
-                .border(.cyan, width: 1)
-                .padding()
-            Spacer()
-            Spacer()
-            HStack {
-                Button("Awesome") {
-                    strMessage="You are Awesome!"
-                }
-                .buttonStyle(.borderedProminent)
-                Spacer()
+        
+        ZStack {
+            Rectangle()
+//                .fill(.yellow.gradient)
+                .fill(
+                    Gradient(colors: [.white, .orange, .black])
+                )
+                .ignoresSafeArea()
+            
+//            Color.yellow.opacity(0.7)
+//                .ignoresSafeArea()
                 
-                Button("Wonderful", action: {strMessage="You are wonderful"})
-                    .buttonStyle(.borderedProminent)
+            VStack (spacing: 4) {
+                Text("You have Skills!")
+                    .font(.largeTitle)
+                    .fontWeight(.black)
+                    .foregroundColor(Color("Gold-BC"))
+                    .background(Color("Maroon-BC"))
+                    .cornerRadius(10)
                 Spacer()
-                Button("Clear") {
-                    strMessage = "This hopefully should be an extra long sentence. If you think it is not then you will have to properly explain in detail why it is not"
+                Text(strMessage)
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
+                    .minimumScaleFactor(0.5)
+                    .multilineTextAlignment(.center)
+                    .foregroundColor(Color.green)
+                    .frame(height: 150)
+                    .frame(maxWidth: .infinity)
+    //                .border(.cyan, width: 1)
+                    .padding()
+                Spacer()
+                Spacer()
+                HStack {
+                    Button("Awesome") {
+                        strMessage="You are Awesome!"
+                    }
+                    .foregroundColor(Color.yellow).bold()
+                    .buttonStyle(.borderedProminent)
+                    Spacer()
+                    
+                    Button("Wonderful", action: {strMessage="You are wonderful"})
+                        .foregroundColor(Color.black)
+                        .buttonStyle(.borderedProminent)
+                    Spacer()
+                    Button("Clear") {
+                        strMessage = "This hopefully should be an extra long sentence. If you think it is not then you will have to properly explain in detail why it is not"
+                    }
+                    .foregroundColor(Color.yellow).bold()
+                    .buttonStyle(.borderedProminent)
                 }
+                .padding()
+                .tint(Color("Vermillion"))
+    //            .background(Color("SkyBlue"))
+    //            .border(.gray, width: 1)
             }
-            .border(.gray, width: 1)
-            Spacer()
         }
     }
 }
