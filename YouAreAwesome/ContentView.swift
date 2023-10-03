11138da2d0ce371c9dc599ae860cfcc765eb92eb
Likhith Areekkal <li.areekkal@gmail.com>
2023-10-03 15:44:12 -0400

    2023-10-03-03:43-Ch1.11

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -6,49 +6,50 @@
 //
 
 import SwiftUI
+import UIKit
+import Foundation
 
 struct ContentView: View {
-    @State private var strMessage = ""
-
+    @State private var varImage = "image"
+    @State private var count: Int = 0
+    @State private var strCount = "0"
+//    @State private var strImgCount = ""
     
     var body: some View {
                 
             VStack (spacing: 4) {
 
-                Spacer()
                 
-                Text(strMessage)
-                    .font(.largeTitle)
-                    .fontWeight(.heavy)
-                    .minimumScaleFactor(0.5)
-                    .multilineTextAlignment(.center)
-                    .foregroundColor(Color.red)
-                    .frame(height: 150)
-                    .frame(maxWidth: .infinity)
+                Spacer()
+//      this doesn't work, why?
+//             strImgCount = "image\(strCount)"
+                Image(varImage+strCount)
+                    .resizable()
+                    .scaledToFit()
+                    .cornerRadius(30)
+                    .shadow(radius: 10)
                     .padding()
                 
-                Spacer()
+                Text(String(count))
                 
                 HStack {
                     
                     Button("Awesome") {
-//                        if (strMessage == message1) {
-//                            strMessage = message2
-//                        }
-//                        else {
-//                            strMessage = message1
-//                        }
-                        let  message1 = "You are Awesome!"
-                        let  message2 = "You are Great!"
-                        strMessage = (strMessage == message1 ? message2 : message1   )
+                        if(count == 9){
+                            count = 0
+                        }
+                        else{
+                            count+=1
+                        }
+                        strCount = String(count)
+                        print(String(count))
                     }
                     .foregroundColor(Color.yellow).bold()
                     .buttonStyle(.borderedProminent)
-                    
- 
                 }
                 .padding()
                 .tint(Color("Vermillion"))
+                Spacer()
             }
 
     }
